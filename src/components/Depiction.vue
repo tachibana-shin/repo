3caddfc147c2d299ee8ffe064f540825bffa7c31
Tachibana Shin <thanhnguyennguyen1995@gmail.com>
2021-12-06 22:11:37 +0700

    Update Depiction.vue

--- src/components/Depiction.vue
+++ src/components/Depiction.vue
@@ -60,9 +60,6 @@
     v-html="message.html"
     v-if="message"
   />
-  
-  <!-- rate widget -->
-  <div class="rw-ui-container" />
 
   <!-- check support -->
   <div>
@@ -142,39 +139,6 @@
   <!-- <SocialShare /> -->
   <ThemeChange />
   <Copyright />
-  <component is="script" type="text/javascript">(function(d, t, e, m){
-    
-    // Async Rating-Widget initialization.
-    window.RW_Async_Init = function(){
-                
-        RW.init({
-            huid: "480790",
-            uid: "c3b308cee2505c84f105fc3153bdb011",
-            source: "website",
-            options: {
-                "advanced": {
-                    "layout": {},
-                    "font": {}
-                },
-                "size": "",
-                "style": "flat_blue",
-                "isDummy": false
-            } 
-        });
-        RW.render();
-    };
-        // Append Rating-Widget JavaScript library.
-    var rw, s = d.getElementsByTagName(e)[0], id = "rw-js",
-        l = d.location, ck = "Y" + t.getFullYear() + 
-        "M" + t.getMonth() + "D" + t.getDate(), p = l.protocol,
-        f = ((l.search.indexOf("DBG=") > -1) ? "" : ".min"),
-        a = ("https:" == p ? "secure." + m + "js/" : "js." + m);
-    if (d.getElementById(id)) return;              
-    rw = d.createElement(e);
-    rw.id = id; rw.async = true; rw.type = "text/javascript";
-    rw.src = p + "//" + a + "external" + f + ".js?ck=" + ck;
-    s.parentNode.insertBefore(rw, s);
-    }(document, new Date(), "script", "rating-widget.com/"));</component>
 </template>
 
 <script lang="ts" setup>
